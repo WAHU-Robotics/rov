6e8e611ed9f11150912bd5d7862cf4b9475d1172
jbuelow1 <jdbuelow@gmail.com>
2018-12-14 12:05:14 -0600

    Ok, so my previous code was garbage. This is someone elses code. Enjoy.

--- wet/src/main/java/me/jbuelow/rov/wet/Wet.java
+++ wet/src/main/java/me/jbuelow/rov/wet/Wet.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //TODO: java vlc embedded thingy
+//this is good code.
 
 /**
  * @author Brian Wachsmuth
