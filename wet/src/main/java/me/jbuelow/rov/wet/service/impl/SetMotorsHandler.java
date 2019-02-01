e0e5af1b7a4079ed738a9acf76c1a4b6cdd6cc97
jbuelow1 <jdbuelow@gmail.com>
2019-01-31 22:00:14 -0600

    almost have theoretical pwm output

--- wet/src/main/java/me/jbuelow/rov/wet/service/impl/SetMotorsHandler.java
+++ wet/src/main/java/me/jbuelow/rov/wet/service/impl/SetMotorsHandler.java
@@ -3,12 +3,14 @@
  */
 package me.jbuelow.rov.wet.service.impl;
 
+import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
 import lombok.extern.slf4j.Slf4j;
 import me.jbuelow.rov.common.MotorPower;
 import me.jbuelow.rov.common.Response;
 import me.jbuelow.rov.common.SetMotors;
 import me.jbuelow.rov.common.SetMotorsResponse;
 import me.jbuelow.rov.wet.service.CommandHandler;
+import me.jbuelow.rov.wet.vehicle.hardware.PCA9685;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,18 +21,32 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SetMotorsHandler implements CommandHandler<SetMotors> {
 
+  PCA9685 driver;
+
+  public SetMotorsHandler() {
+    try {
+      driver = new PCA9685();
+    } catch (UnsupportedBusNumberException e) {
+      e.printStackTrace();
+    }
+  }
+
   /* (non-Javadoc)
    * @see net.wachsmuths.rov.service.CommandHandler#execute(Command)
    */
   @Override
   public Response execute(SetMotors command) {
     log.debug("Got Set Motors Command!");
-    int i = 0;
     for (MotorPower motor : command.getPowerLevels()) {
-      log.info("Motor " + i + " power: " + motor.getPower());
-      i++;
+      log.info("Motor " + motor.getId() + " power: " + motor.getPower());
+
+      driver.setServoPulse(motor.getId(), convertToPulse(motor.getPower()));
     }
     return new SetMotorsResponse(true);
+  }
+
+  private float convertToPulse(int power) {
+    return ((float) power / 1000f) + 1.5f;
   }
 
   /* (non-Javadoc)
