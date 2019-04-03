4f0b3a9aaed8d4da037696ab0bf84e14319ef417
jbuelow1 <jdbuelow@gmail.com>
2019-04-03 14:12:18 -0500

    filled in axes vars

--- common/src/main/java/me/jbuelow/rov/common/object/MotionVector.java
+++ common/src/main/java/me/jbuelow/rov/common/object/MotionVector.java
@@ -2,4 +2,12 @@ package me.jbuelow.rov.common.object;
 
 public class MotionVector extends ROVObject {
 
+  // 6-DoF axes
+  int surge;
+  int sway;
+  int heave;
+  int pitch;
+  int yaw;
+  int roll;
+
 }
