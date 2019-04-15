98095e2a51f4c3e62730f39853d42f9c4f2b2cf0
Ian-Luedke <47649424+Ian-Luedke@users.noreply.github.com>
2019-04-14 23:33:07 -0500

    I an adding a Shutdown command, with the supervision of Jacob

---
+++ common/src/main/java/me/jbuelow/rov/common/command/Shutdown.java
@@ -0,0 +1,14 @@
+package me.jbuelow.rov.common.command;
+
+import lombok.Getter;
+
+public class Shutdown extends Command {
+    private static final long serialVersionUID = 4207802681924667139L;
+
+    @Getter
+    private boolean reboot;
+
+    public Shutdown(boolean reboot) {
+        this.reboot = reboot;
+    }
+}
