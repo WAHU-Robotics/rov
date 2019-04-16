cbdcb0077ea60e171ed65f2e1851ecc62ae0f19a
jbuelow1 <jdbuelow@gmail.com>
2019-04-16 13:31:57 -0500

    fixed build

--- dry/src/main/java/me/jbuelow/rov/dry/service/impl/VehicleControlServiceImpl.java
+++ dry/src/main/java/me/jbuelow/rov/dry/service/impl/VehicleControlServiceImpl.java
@@ -11,9 +11,7 @@ import me.jbuelow.rov.common.command.Command;
 import me.jbuelow.rov.common.response.Response;
 import me.jbuelow.rov.common.response.VehicleCapabilities;
 import me.jbuelow.rov.dry.discovery.VehicleDiscoveryEvent;
-import me.jbuelow.rov.dry.exception.JinputNativesNotFoundException;
 import me.jbuelow.rov.dry.service.VehicleControlService;
-import me.jbuelow.rov.dry.ui.error.GeneralError;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -96,10 +94,6 @@ public class VehicleControlServiceImpl implements VehicleControlService {
       event.setVehicleID(handler.getId());
     } catch (ClassNotFoundException | IOException e) {
       log.error("Error opening communications to vehicle", e);
-    } catch (JinputNativesNotFoundException e) {
-      GeneralError.display("<html><b>An exception occoured: " + e.getClass().getSimpleName() +
-          "</b><br>Ensure that the required natives are either<br>in a subdirectory of the working directory,<br>or add them to $PATH.</html>");
-      System.exit(1);
     }
   }
 }
