5227271b0fe9caefafd5c06083d1a6e8b2ad9698
jdbuelow1 <jdbuelow@gmail.com>
2020-01-23 12:45:15 -0600

    added ImuDevice sensor device type

---
+++ wet/src/main/java/me/jbuelow/rov/wet/vehicle/hardware/i2c/sensor/imu/ImuDevice.java
@@ -0,0 +1,30 @@
+package me.jbuelow.rov.wet.vehicle.hardware.i2c.sensor.imu;
+
+import com.pi4j.io.i2c.I2CFactory.UnsupportedBusNumberException;
+import java.io.IOException;
+import java.util.Vector;
+import javax.measure.Quantity;
+import javax.measure.quantity.Acceleration;
+import javax.measure.quantity.AngularVelocity;
+import javax.measure.quantity.MagneticFluxDensity;
+import javax.measure.quantity.Temperature;
+import me.jbuelow.rov.common.object.imu.Orientation;
+
+public interface ImuDevice {
+
+  void initialize() throws IOException, UnsupportedBusNumberException;
+
+  Vector<Quantity<Acceleration>> getAccelerationVector();
+
+  Vector<Quantity<Acceleration>> getLinearAccelerationVector();
+
+  Vector<Quantity<Acceleration>> getGravityVector();
+
+  Orientation getAbsoluteRotation() throws IOException;
+
+  Vector<AngularVelocity> getAngularVelocity();
+
+  Vector<Quantity<MagneticFluxDensity>> getMagneticField();
+
+  Quantity<Temperature> getTemperature();
+}
