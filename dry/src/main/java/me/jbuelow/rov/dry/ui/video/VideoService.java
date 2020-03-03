405668f5ef2eff6ff279e6dbea7930e68b23a376
jdbuelow1 <jdbuelow@gmail.com>
2020-03-03 13:07:36 -0600

    set video stream address back to config file

--- dry/src/main/java/me/jbuelow/rov/dry/ui/video/VideoService.java
+++ dry/src/main/java/me/jbuelow/rov/dry/ui/video/VideoService.java
@@ -3,6 +3,7 @@ package me.jbuelow.rov.dry.ui.video;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import me.jbuelow.rov.dry.discovery.VehicleDiscoveryEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ public class VideoService {
 
   private final VideoFrameReceiver frameReceiver;
   private VideoTransport transport;
+
+  @Value("${rov.camera.url}")
   private String videoUrl = "udp://239.0.0.1:1234";
 
   public VideoService(VideoFrameReceiver frameReceiver) {
