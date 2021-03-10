58558d6833afc8e5ca32e216b4ab0cb5c5aec43f
jbuelow1 <jdbuelow@gmail.com>
2021-03-10 11:47:37 -0600

    Redid some things to make camera streaming async from rest of software. tested as working with vlc media player v2.2.1 using udp://@239.0.0.1:1234 as the address

--- wet/src/main/java/me/jbuelow/rov/wet/service/camera/WebCam.java
+++ wet/src/main/java/me/jbuelow/rov/wet/service/camera/WebCam.java
@@ -1,5 +1,6 @@
 package me.jbuelow.rov.wet.service.camera;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.javacv.CanvasFrame;
 import org.bytedeco.javacv.FFmpegFrameRecorder;
 import org.bytedeco.javacv.Frame;
@@ -7,33 +8,86 @@ import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.FrameGrabber.Exception;
 import org.bytedeco.javacv.FrameRecorder;
 import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Service
 public class WebCam {
 
   final int width = 1280;
   final int height = 720;
 
-  public void start() throws Exception, FrameRecorder.Exception {
-    CanvasFrame frame = new CanvasFrame("Video Test");
-    FrameGrabber grabber = new OpenCVFrameGrabber(0);
+  private FrameGrabber grabber;
+  private FFmpegFrameRecorder recorder;
+  private StreamThread thread;
+
+  @PostConstruct
+  public void start() {
+    log.info("Starting camera stream...");
+
+    grabber = new OpenCVFrameGrabber(0);
     grabber.setImageHeight(height);
     grabber.setImageWidth(width);
-    grabber.start();
-    Frame image = grabber.grab();
-    FFmpegFrameRecorder recorder = new FFmpegFrameRecorder("udp://239.0.0.1:1234?ttl=130", width, height);
+    try {
+      grabber.start();
+    } catch (java.lang.Exception e) {
+      log.error("Failed to start camera stream!", e);
+      return;
+    }
+    log.debug("Opened camera frame grabber");
+
+    recorder = new FFmpegFrameRecorder("udp://239.0.0.1:1234?ttl=130", width, height);
     recorder.setVideoCodecName("h264");
     recorder.setFormat("mpegts");
     recorder.setFrameRate(30);
     recorder.setVideoBitrate(10000000);
-    recorder.start();
-
-    while ((image = grabber.grab()) != null) {
-      frame.showImage(image);
-      recorder.record(image);
+    try {
+      recorder.start();
+    } catch (java.lang.Exception e) {
+      log.error("Failed to start camera stream!", e);
+      return;
     }
-    frame.dispose();
-    recorder.stop();
-    grabber.stop();
+    log.debug("Opened stream frame recorder");
+
+    thread = new StreamThread();
+
+    thread.start();
+    log.info("Camera stream started!");
   }
 
+  private class StreamThread extends Thread {
+
+    @Override
+    public void run() {
+      Frame image = null;
+      try {
+        image = grabber.grab();
+      } catch (Exception e) {
+        log.error("Error grabbing initial frame!", e);
+      }
+
+      while (true) {
+        try {
+          if ((image = grabber.grab()) == null) break;
+        } catch (Exception e) {
+          log.error("Error grabbing frame!", e);
+        }
+        try {
+          recorder.record(image);
+        } catch (FrameRecorder.Exception e) {
+          log.error("Error recording frame!", e);
+        }
+      }
+
+      try {
+        recorder.stop();
+        grabber.stop();
+      } catch (Exception | FrameRecorder.Exception e) {
+        log.error("Error occurred while stopping stream:", e);
+      }
+    }
+
+  }
 }
