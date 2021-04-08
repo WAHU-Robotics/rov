56a21cdcdab5dd80f7a0554cd606a52ac777299e
jbuelow1 <jdbuelow@gmail.com>
2021-04-08 14:14:25 -0500

    implemented camera feed scaling

--- dry/src/main/java/org/snapshotscience/rov/dry/ui/video/VideoPane.java
+++ dry/src/main/java/org/snapshotscience/rov/dry/ui/video/VideoPane.java
@@ -1,12 +1,12 @@
 package org.snapshotscience.rov.dry.ui.video;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import lombok.Getter;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.swing.JPanel;
-import lombok.Getter;
 
 public class VideoPane extends JPanel {
 
@@ -22,7 +22,19 @@ public class VideoPane extends JPanel {
       //LOS is drawn here
       //TODO make LOS screen
     } else {
-      g2.drawImage(image, 0, 0, null);
+      //scale image
+      double widthRatio = (double) getSize().width / image.getWidth();
+      double heightRatio = (double) getSize().height / image.getHeight();
+      Image scaled;
+      if (widthRatio > heightRatio) {
+        int width = (int) (image.getWidth() * ((double) getSize().height / image.getHeight()));
+        scaled = image.getScaledInstance(width, getSize().height, BufferedImage.TYPE_INT_ARGB);
+      } else {
+        int height = (int) (image.getHeight() * ((double) getSize().width / image.getWidth()));
+        scaled = image.getScaledInstance(getSize().width, height, BufferedImage.TYPE_INT_ARGB);
+      }
+
+      g2.drawImage(scaled, 0, 0, null);
     }
   }
 
